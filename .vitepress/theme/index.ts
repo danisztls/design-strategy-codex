0f48b1d6cf22693ee02c116dda51082d26b2c978
Daniel Souza <me@posix.dev.br>
2026-01-07 18:34:59 -0300

    Remover router code
    Not sure if this code is useful here.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -11,19 +11,6 @@ const customTheme: Theme = {
 	Layout: CustomLayout,
 	enhanceApp( { app, router } ) {
 		app.component( 'TeamGrid', TeamGrid );
-
-		// If we went to a URL that matches a redirect entry, go to the redirected URL instead.
-		router.onAfterRouteChanged = async ( to ) => {
-			for ( const [ redirFrom, redirTo ] of Object.entries( redirects ) ) {
-				if ( to.endsWith( redirFrom ) ) {
-					// router.go() wants a full URL with domain name etc. Instead of trying to
-					// construct that ourselves, obtain it by doing a replace on `to` (which is
-					// also a full URL).
-					await router.go( to.replace( redirFrom, redirTo ) );
-					break;
-				}
-			}
-		};
 	}
 };
 
