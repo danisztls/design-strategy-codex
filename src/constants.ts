09b70ed69c655f18de778551809a5fd6fa971226
Daniel Souza <me@posix.dev.br>
2026-01-07 19:19:55 -0300

    Clean up code irrelevant to this project scope

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionKey, Ref } from 'vue';
-import { HTMLDirection } from '@wikimedia/codex';
-
-export const DirectionKey: InjectionKey<Ref<HTMLDirection>> = Symbol( 'CdxDemoDirection' );
-export const UpdateTeleportTargetDirKey: InjectionKey<( newDir: HTMLDirection ) => void> = Symbol( 'CdxDemoUpdateTeleportTargetDir' );
