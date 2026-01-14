a97da9f2a8f2673b4dcaabdff17287223ba54675
Daniel Souza <me@posix.dev.br>
2026-01-14 00:07:57 -0300

    Clean up

--- .vitepress/types.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultTheme } from 'vitepress';
-
-// We want to pass some config data to the CustomLayout component, so we need to extend VitePress's
-// DefaultTheme.Config type.
-export interface CustomConfig extends DefaultTheme.Config {
-	isMainBranch: boolean,
-	isBranchDeploy: boolean,
-	patchID?: string
-}
