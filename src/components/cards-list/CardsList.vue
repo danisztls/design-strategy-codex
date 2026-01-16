0b57497b61fc1d477d3a3135ec755aeb33465125
Daniel Souza <me@posix.dev.br>
2026-01-16 00:21:35 -0300

    CardsList: Improve

--- src/components/cards-list/CardsList.vue
+++ src/components/cards-list/CardsList.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { CdxCard } from '@wikimedia/codex';
+import { CdxCard, CdxIcon } from '@wikimedia/codex';
+import { externalLinkIcon } from '../../utils/custom-icons';
 
 const props = defineProps({
   list: {
@@ -7,6 +8,11 @@ const props = defineProps({
     required: true
   }
 })
+
+function isExternalUrl(url?: string): boolean {
+  if (!url) return false;
+  return /^(https?:)?\/\//.test(url);
+}
 </script>
 
 <template>
@@ -15,10 +21,17 @@ const props = defineProps({
 		v-for="item in list"
 		:key="item.title"
 		:url="`${item.url}`"
+		:target="isExternalUrl(item.url) ? '_blank' : undefined"
+		:rel="isExternalUrl(item.url) ? 'noopener noreferrer' : undefined"
 		clickable
 	>
 		<template #title>
+		  <span v-if="isExternalUrl(item.url)" class="card-title-with-icon">
+			{{ item.title }} <cdx-icon :icon="externalLinkIcon" size="small" />
+		  </span>
+		  <span v-else>
 			{{ item.title }}
+		  </span>
 		</template>
 
 		<template #description>
@@ -33,5 +46,29 @@ const props = defineProps({
 	display: grid;
 	grid-template-columns: repeat(auto-fill, minmax(260px, 1fr));
 	gap: var(--spacing-100);
+}
+
+:deep(.cdx-card__text) {
+	width: 100%
+}
+
+:deep(.card-title-with-icon) {
+	display: inline-flex;
+    justify-content: space-between;
+	align-items: center;
+	gap: 1em;
+	width: 100%;
+}
+
+.cdx-card {
+	:deep(.cdx-icon) {
+		color:	var(--color-disabled);
+	}
+
+	&:hover, &:focus-within {
+		:deep(.cdx-icon) {
+			color:	var(--color-hover);
+		}
+	}
 }
 </style>
