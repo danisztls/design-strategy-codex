3e1bc1039f5dc0c8a9882b668adeff3559c019b7
Daniel Souza <me@posix.dev.br>
2026-01-22 09:54:43 -0300

    CardsList: Refactor

--- src/components/cards-list/CardsList.vue
+++ src/components/cards-list/CardsList.vue
@@ -1,44 +1,53 @@
 <script setup lang="ts">
+import type { PropType } from 'vue';
 import { CdxCard, CdxIcon } from '@wikimedia/codex';
 import { ICONS } from '../../utils/icons'
 
+type CardItem = {
+	title: string;
+	description?: string;
+	url?: string;
+};
+
 const props = defineProps({
-  list: {
-    type: Array,
-    required: true
-  }
-})
+	list: {
+		type: Array as PropType<CardItem[]>,
+		required: true
+	}
+});
 
 function isExternalUrl(url?: string): boolean {
-  if (!url) return false;
-  return /^(https?:)?\/\//.test(url);
+	if (!url) return false;
+	return /^(https?:)?\/\//.test(url);
 }
+
 </script>
 
 <template>
-<div class="cards-list">
-	<cdx-card
-		v-for="item in list"
-		:key="item.title"
-		:url="`${item.url}`"
-		:target="isExternalUrl(item.url) ? '_blank' : undefined"
-		:rel="isExternalUrl(item.url) ? 'noopener noreferrer' : undefined"
-		clickable
-	>
-		<template #title>
-		  <span v-if="isExternalUrl(item.url)" class="card-title-with-icon">
-			{{ item.title }} <cdx-icon :icon="ICONS['externalLink']" size="small" />
-		  </span>
-		  <span v-else>
-			{{ item.title }}
-		  </span>
-		</template>
+	<div class="cards-list">
+		<CdxCard
+			v-for="item in props.list"
+			:key="item.title"
+			:url="item.url"
+			:target="isExternalUrl(item.url) ? '_blank' : undefined"
+			:rel="isExternalUrl(item.url) ? 'noopener noreferrer' : undefined"
+			clickable
+		>
+			<template #title>
+				<span v-if="isExternalUrl(item.url)" class="card-title-with-icon">
+					{{ item.title }}
+					<CdxIcon :icon="ICONS['externalLink']" size="small" />
+				</span>
+				<span v-else>
+					{{ item.title }}
+				</span>
+			</template>
 
-		<template #description>
-			{{ item.description }}
-		</template>
-	</cdx-card>
-</div>
+			<template #description>
+				{{ item.description }}
+			</template>
+		</CdxCard>
+	</div>
 </template>
 
 <style scoped>
@@ -61,8 +70,11 @@ function isExternalUrl(url?: string): boolean {
 }
 
 .cdx-card {
+	transition: border 250ms ease;
+
 	:deep(.cdx-icon) {
 		color:	var(--color-disabled);
+		transition: color 330ms ease;
 	}
 
 	&:hover, &:focus-within {
