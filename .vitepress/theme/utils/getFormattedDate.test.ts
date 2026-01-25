991ba4474e09779538683658942df9510036fbbe
Daniel Souza <me@posix.dev.br>
2026-01-24 23:02:59 -0300

    Remove test cases
    Not worth the extra complexity

--- .vitepress/theme/utils/getFormattedDate.test.ts
+++
@@ -1,7 +0,0 @@
-import getFormattedDate from './getFormattedDate';
-
-describe( 'getFormattedDate', () => {
-	it( 'returns properly formatted date', () => {
-		expect( getFormattedDate( 1703743920000 ) ).toBe( '06:12, 2023-12-28' );
-	} );
-} );
--- .vitepress/theme/utils/toKebabCase.test.ts
+++
@@ -1,7 +0,0 @@
-import toKebabCase from './toKebabCase';
-
-describe( 'toKebabCase', () => {
-	it( 'converts component names', () => {
-		expect( toKebabCase( 'CdxToggleButton' ) ).toBe( 'cdx-toggle-button' );
-	} );
-} );
